642f9dd25c1a10eeead99f04857f4f7b52699baf
Felipe B Carbonell <fbcarbonell22@gmail.com>
2024-05-03 14:52:19 -0300

    início de Avalanche

--- constantes.py
+++ constantes.py
@@ -2,6 +2,39 @@ import random, math, time, pygame
 
 # Tela:
 
-Heigth = 0
-Length = 0
+Heigth = 4 * 250
+Length = 3 * 250
 
+# Situações:
+
+# Trem esquerda = -1
+# Trem meio = 0
+# Trem direita = 1
+# Sem trem = 2
+
+#situação 1 exemplo:
+#   [
+#        [-1, 2, 2],
+#        [-1, 2, 2],
+#        [0, 2, 1],
+#        [2, 2, 1],
+#        [2, 2, 0],
+#        [2, 2, 0]
+#   ]
+#       ,█     ,
+#       ,█     ,
+#       ,██   █,
+#       ,     █,
+#       ,    ██,
+#       ,    ██,
+
+situations = [
+    [
+        [-1, 2, 2],
+        [-1, 2, 2],
+        [0, 2, 1],
+        [2, 2, 1],
+        [2, 2, 0],
+        [2, 2, 0]
+    ] 
+]
