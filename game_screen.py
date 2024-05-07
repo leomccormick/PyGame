b22ce6860daa18ff47948537eb0d249847bbc44f
Leo <leonardovm2@al.insper.edu.br>
2024-05-06 21:37:38 -0300

    basico

---
+++ game_screen.py
@@ -0,0 +1,13 @@
+from constantes import *
+import pygame
+
+def main():
+    game = True
+
+    while game:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                game = False
+                break
+
+    pygame.quit()
---
+++ main.py
@@ -0,0 +1,9 @@
+import pygame
+from constantes import *
+from game_screen import main
+
+WINDOW = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("Avalanche")
+
+if __name__ == "__main__":
+    main()
