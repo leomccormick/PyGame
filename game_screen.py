be5bfa93f73698c3d09d6057a22d6a5fcd9aa23f
Felipe B Carbonell <fbcarbonell22@gmail.com>
2024-05-10 15:01:44 -0300

    gamescreeninit

--- game_screen.py
+++ game_screen.py
@@ -1,29 +1,53 @@
 from config import *
 from assets import load_assets
+from sprites import *
 import pygame
 
 def main():
-    game = True
+    clock = pygame.time.Clock()
 
     assets = load_assets()
 
-    state = GAME
+    all_sprites = pygame.sprite.Group()
+    all_ice = pygame.sprite.Group()
+
+    groups = {}
+    groups['all_sprites'] = all_sprites
+    groups['all_ice'] = all_ice
+
+    player = Player(groups, assets)
+    all_sprites.add(player)
+
+    DONE = 0
+    PLAYING = 1
+    state = PLAYING
     
     keys_down = {}
 
-    while game:
+    score = 0
+    fase = 1
+
+    while state != DONE:
+        clock.tick(FPS)
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
-                game = False
-                break
-            if state == GAME:
+                state = DONE
+            if state == PLAYING:
                 if event.type == pygame.KEYDOWN:
                     keys_down[event.key] = True
                     if event.key == pygame.K_LEFT:
-                        player.aceleracao = -1562
+                        if player.parado and player.x != 625:
+                            player.aceleracao = -125*2/0.09
+                            player.parado = False
                     if event.key == pygame.K_RIGHT:
-                        player.aceleracao = 1562
+                        if player.parado and player.x != 125:
+                            player.aceleracao = 125*2/0.09
+                            player.parado = False
 
-                
-
+        all_sprites.update()
+        
+        if state == PLAYING:
+            hits = pygame.sprite.spritecollide(player, all_ice, False, pygame.sprite.collide_mask)
+        if len(hits) > 0:
+            now = pygame.time.get_ticks()
     pygame.quit()
