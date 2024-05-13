69546ab26a9d02ec383f9a165bd4be4319242a19
Felipe B Carbonell <fbcarbonell22@gmail.com>
2024-05-13 10:25:01 -0300

    Iceclass

--- sprites.py
+++ sprites.py
@@ -1,3 +1,4 @@
+from pygame.sprite import _Group
 from config import *
 
 class Player(pygame.sprite.Sprite):
@@ -22,3 +23,26 @@ class Player(pygame.sprite.Sprite):
             if self.rect.x % (WIDTH/3 - WIDTH/6) == 0:
                 self.acaeleracao *= 0
                 self.rect.x *= 0
+
+class Ice(pygame.sprite.Sprite):
+    def __init__(self, groups, assets, lane):
+        pygame.sprite.Sprite.__init__(self)
+
+        self.image = assets[ICE_IMG]
+        self.mask = pygame.mask.from_surface(self.image)
+        self.rect = self.image.get_rect()
+        if lane == 0:
+            self.rect.x = 125
+        elif lane == 1:
+            self.rect.x = 375
+        elif lane == 2:
+            self.rect.x = 600
+        self.rect.y = 0
+        self.speed = 50
+        self.groups = groups
+        self.assets = assets
+    
+    def update(self):
+        self.rect.y += self.speed
+        if self.rect.y <= HEIGHT:
+            self.kill()
