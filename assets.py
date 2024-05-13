e88574cf2193141282884c3dbc227374098e9dd3
Inacio Quaresma <inacioqa@al.insper.edu.br>
2024-05-13 10:10:02 -0300

    imagem

--- assets.py
+++ assets.py
@@ -7,7 +7,7 @@ BACKGROUND = 'background'
 def load_assets():
     assets = {}
     assets[BACKGROUND] = pygame.image.load(os.path.join(IMG_DIR, 'background.jpg')).convert()
-    
+
     # Carrega os sons do jogo
     pygame.mixer.music.load(os.path.join(SND_DIR, 'tgfcoder-FrozenJam-SeamlessLoop.ogg'))
     pygame.mixer.music.set_volume(0.4)
